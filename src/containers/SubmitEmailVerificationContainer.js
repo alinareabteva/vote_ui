d3fa76187440c704a7e7d457428c9e8af18369f6
alinareabteva <52407474+alinareabteva@users.noreply.github.com>
2022-05-23 16:06:34 +0300

    Congratulation Page notification

--- src/containers/SubmitEmailVerificationContainer.js
+++ src/containers/SubmitEmailVerificationContainer.js
@@ -5,6 +5,27 @@ import {Spinner} from "reactstrap";
 import {Button} from "@mui/material";
 import {ROUTES_PATHS} from "../layout/routes-constants";
 
+const bodyStyle = {
+    background: '#fff4a3',
+    marginTop: '10%',
+}
+const text1Style = {
+    fontFamily: 'Raleway, Arial',
+    fontSize: 34,
+    textAlign: 'center',
+    fontWeight: 'bold'
+
+}
+const text2Style = {
+    fontFamily: 'Raleway, Arial',
+    fontSize: 26,
+    textAlign: 'center'
+
+}
+const buttonStyle = {
+    left: '46%'
+}
+
 const SubmitEmailVerificationContainer = () => {
     const params = useParams();
     const navigate = useNavigate();
@@ -38,16 +59,17 @@ const SubmitEmailVerificationContainer = () => {
     const onClickGoToSignIn = () => {
         navigate(ROUTES_PATHS.LOGIN_PAGE);
     }
-    // TODO: fix visual
+
     return (
-        <div>
+        <div style={bodyStyle}>
             {state.loading && <Spinner/>}
             {!state.loading && !!state.registrationRequest && (
-                <div className="success-submitting">
-                    <h1>Congratulations</h1>
-                    <p>Your email has been confirmed and registration is finished.</p>
-                    <p> Now you're able to Sign in </p>
-                    <Button onClick={onClickGoToSignIn}>Go to Sign In</Button>
+
+                <div class="success-submitting">
+                    <h1 style={text1Style}>Congratulations!</h1>
+                    <p style={text2Style}>Your email has been confirmed and registration is finished.</p>
+                    <p style={text2Style}> Now you're able to Sign in </p>
+                    <Button variant="contained" color="success" style={buttonStyle} onClick={onClickGoToSignIn}>Go to Sign In</Button>
                 </div>
             )}
             {state.errors && state.errors}
