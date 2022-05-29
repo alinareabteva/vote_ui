5ac1e5e7d355882c4d6b78b067ba74202d4758d4
Vasilii Bolbecov <vbolbecov@complexica.com>
2022-05-29 18:57:27 +0300

    Fix avatar name display

--- src/layout/MainLayout/Heading.js
+++ src/layout/MainLayout/Heading.js
@@ -143,7 +143,7 @@ export const Heading = () => {
                     <Box sx={{flexGrow: 0}}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"/>
+                                <Avatar alt={currentUser?.firstName} src="/static/images/avatar/2.jpg"/>
                             </IconButton>
                         </Tooltip>
                         <Menu
